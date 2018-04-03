3483f98b8c168768975b577bdbb87271145f5cfe
Vadim <burdinvadim@gmail.com>
2018-04-03 10:51:00 +0300

    Меняем class на className

--- hw3/src/client/app/components/footer.jsx
+++ hw3/src/client/app/components/footer.jsx
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 
 export default class Footer extends Component {
 	render() {
-	    return <footer class="">
-			<ul class=" navbar navbar-dark bg-dark pagination pagination-lg justify-content-center">
-				<li class="page-item"><a class="page-link" href="#">Previous</a></li>
-				<li class="page-item"><a class="page-link" href="#">1</a></li>
-				<li class="page-item active "><a class="page-link" href="#">2</a></li>
-				<li class="page-item"><a class="page-link" href="#">3</a></li>
-				<li class="page-item"><a class="page-link" href="#">Next</a></li>
+	    return <footer className="">
+			<ul className=" navbar navbar-dark bg-dark pagination pagination-lg justify-content-center">
+				<li className="page-item"><a className="page-link" href="#">Previous</a></li>
+				<li className="page-item"><a className="page-link" href="#">1</a></li>
+				<li className="page-item active "><a className="page-link" href="#">2</a></li>
+				<li className="page-item"><a className="page-link" href="#">3</a></li>
+				<li className="page-item"><a className="page-link" href="#">Next</a></li>
 			</ul>
-		</footer>;
+		Name</footer>;
 	}
 }
--- hw3/src/client/app/components/header.jsx
+++ hw3/src/client/app/components/header.jsx
@@ -3,69 +3,69 @@ import React, { Component } from 'react';
 export default class Footer extends Component {
 	render() {
 	    return (<header>
-	        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-			    <a class="navbar-brand" href="https://reactjs.org/" target= "_blank">
+	        <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+			    <a className="navbar-brand" href="https://reactjs.org/" target= "_blank">
 					<img src="./img/react.png" alt="logo"/>
                 </a>
-		        <ul class="navbar-nav">
-					<li class="nav-item">
-						<a class="nav-link" href="https://reactjs.org/docs/introducing-jsx.html" target= "_blank">JSX</a>
+		        <ul className="navbar-nav">
+					<li className="nav-item">
+						<a className="nav-link" href="https://reactjs.org/docs/introducing-jsx.html" target= "_blank">JSX</a>
 					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="https://babeljs.io/" target= "_blank">Babel</a>
+					<li Name="nav-item">
+						<a className="nav-link" href="https://babeljs.io/" target= "_blank">Babel</a>
 					</li>
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+					<li className="nav-item dropdown">
+						<a className="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
 							WEBPACK
 						</a>
-						<div class="dropdown-menu">
-							<a class="dropdown-item" href="https://webpack.js.org/concepts/" target= "_blank">Documentation</a>
-							<a class="dropdown-item" href="https://webpack.js.org/concepts/loaders/" target= "_blank">Loaders</a>
-							<a class="dropdown-item" href="https://webpack.js.org/concepts/modules/" target= "_blank">Modules</a>
+						<div className="dropdown-menu">
+							<a className="dropdown-item" href="https://webpack.js.org/concepts/" target= "_blank">Documentation</a>
+							<a className="dropdown-item" href="https://webpack.js.org/concepts/loaders/" target= "_blank">Loaders</a>
+							<a className="dropdown-item" href="https://webpack.js.org/concepts/modules/" target= "_blank">Modules</a>
 						</div>
 					</li>
 		        </ul>
-				<ul class="nav navbar-nav ml-auto">
-					<li class="nav-item">
-						<form class="form-inline" action="#">
-							<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+				<ul className="nav navbar-nav ml-auto">
+					<li className="nav-item">
+						<form className="form-inline" action="#">
+							<button type="button" className="btn btn-primary" data-toggle="modal" data-target="#myModal">
 								Log In
 							</button>
 						</form>
 					</li>
 				</ul>
 			</nav>
-			<div class="container">
-				<div class="modal fade" id="myModal">
-					<div class="modal-dialog modal-lg">
-						<div class="modal-content">
-							<div class="modal-header">
-								<h4 class="modal-title">Learn ReactJS</h4>
-								<button type="button" class="close" data-dismiss="modal">&times;</button>
+			<div className="container">
+				<div className="modal fade" id="myModal">
+					<div className="modal-dialog modal-lg">
+						<div className="modal-content">
+							<div className="modal-header">
+								<h4 className="modal-title">Learn ReactJS</h4>
+								<button type="button" clasNamesName="close" data-dismiss="modal">&times;</button>
 							</div>
-							<div class="modal-body">
-								<div class="container">
+							<div className="modal-body">
+								<div className="container">
 									<h2>Log In!</h2>
 									<form action="/action_page.php">
-										<div class="form-group">
+										<div className="form-group">
 											<label for="email">USERNAME OR EMAIL</label>
-											<input type="email" class="form-control" id="email" placeholder="Enter username or email" name="email"/>
+											<input type="email" className="form-control" id="email" placeholder="Enter username or email" name="email"/>
 										</div>
-										<div class="form-group">
+										<div className="form-group">
 											<label for="pwd">PASSWORD</label>
-											<input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd"/>
+											<input type="password" className="form-control" id="pwd" placeholder="Enter password" name="pswd"/>
 										</div>
-										<div class="form-check">
-											<label class="form-check-label">
-												<input class="form-check-input" type="checkbox" name="remember"/> Remember me
+										<div className="form-check">
+											<label className="form-check-label">
+												<input className="form-check-input" type="checkbox" name="remember"/> Remember me
 											</label>
 										</div>
-										<button type="submit" class="btn btn-primary">Submit</button>
+										<button type="submit" className="btn btn-primary">Submit</button>
 									</form>
 								</div>
 							</div>
-							<div class="modal-footer">
-								<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+							<div className="modal-footer">
+								<button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
 							</div>
 						</div>
 					</div>
